c6ba6ff2d4ce8ec445344a34a8293a3d6d403e87
Fichtner21 <ernest.fichtner@gmail.com>
2020-02-25 22:27:34 +0100

    test date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-udemy';
+  title = 'Lista Zadań';
+
+  get footer(): string {
+    return ' © Lista zadań, All rights reserved.';
+  }
+
+  getDate(): Date {
+    return new Date();
+  }
+
+  constructor(@Inject(LOCALE_ID) private localeId: string){
+    console.log('Locale: ', this.localeId);
+  }
 }
